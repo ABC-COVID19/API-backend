a2e7f865e68d3ddbb7e4a9c36d61909c3b3e7a1e
Filipe Marcelino <fmarcelino@gmail.com>
2020-03-26 22:55:14 +0000

    Rebuild backend

--- src/main/java/pt/tech4covid/domain/enumeration/ContentSource.java
+++
@@ -1,8 +0,0 @@
-package pt.tech4covid.domain.enumeration;
-
-/**
- * The ContentSource enumeration.
- */
-public enum ContentSource {
-    PubMed
-}
--- src/main/java/pt/tech4covid/domain/enumeration/ReviewState.java
+++
@@ -1,8 +0,0 @@
-package pt.tech4covid.domain.enumeration;
-
-/**
- * The ReviewState enumeration.
- */
-public enum ReviewState {
-    Hold, OnGoing, Pending, Reviewed, Accepted
-}
